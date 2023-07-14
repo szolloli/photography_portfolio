4dc5e487f522467536fbd53e5483b6f60470aa55
szolloli <szolloli@fit.cvut.cz>
2023-07-14 02:07:14 +0200

    Finish gallery

--- script.js
+++ script.js
@@ -2,21 +2,94 @@
 
 //, 'assets/img005.jpg', 'assets/img006.jpg', 'assets/img008.jpg'],
 var images = {
-    'neuart': ['000015.JPG','000017.JPG','000012.JPG','000025.JPG','000028.JPG'],
-    'where_you_are': ['image4.jpg', 'image5.jpg', 'image6.jpg'],
-    'editorial': ['image7.jpg', 'image8.jpg', 'image9.jpg'],
+    'neuart': ['000015.JPG','000012.JPG','000025.JPG','000017.JPG','000028.JPG','img004.jpg','img005.jpg','img015.jpg','img022.jpg','img024.jpg','img044.jpg','img054.jpg','img058.jpg','img080.jpg','img083.jpg','img084.jpg' ],
+    'where_you_are': ['01.jpg', '02.jpg', '23.jpg', '04.jpg', '07.jpg', '08.jpg', '11.jpg', '09.jpg'],
+    'editorial': ['000011.JPG','000010.JPG', '000009.JPG','000011 (1).JPG','000013.JPG','000014.JPG','000015.JPG','000016.JPG','000017.JPG','000018.JPG','000023.JPG','000024.JPG','000028.JPG','000029.JPG'],
+    'protests': ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg'],
     'edge': ['szolloli_01_portret.jpeg','szolloli_02_portret.jpeg','szolloli_03_portret.jpeg','szolloli_04_portret.jpeg',
     'szolloli_05_portret.jpeg','szolloli_06_portret.jpeg','szolloli_07_portret.jpeg',
     'szolloli_09_portret.jpeg','szolloli_10_portret.jpeg']
 };
 
+var about = `
+<div class="fl-rich-text">
+<p class="p1"><span class="s1">Oliver Szöllösi is a Slovak photographer, currently based in Prague, Czech Republic, primarily making work that explores his relationship to places where he grew up and people who he grew up with.</span></p>
+<p>Oliver also explores the boundaries of today's photography through experiments with generative neural networks trained on his own photographs.</p>
+<p>For inquiries, commissions and print sales please email Oliver directly:</p>
+<p class="p1"><b>oliver.szollosi1(at)gmail.com</b></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<h3><strong>Exhibitions</strong></h3>
+<p class="p1"><strong><span class="s1">2022</span></strong></p>
+<p class="p1"><span class="s1">Neuart winter edition</span></p>
+<p class="p1"><strong><span class="s1">2023</span></strong></p>
+<p class="p1"><span class="s1">DIF2023 - CIIRC</span></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<h3 class="p1">Clients</h3>
+<p class="p1">Gangsta Punk, Neuart</p>
+</div>`
 
+var gallery = `
+<div class="real-arrow-content">
+<p data-action="left" class='btn-chevron left bx bx-chevron-left'>
+  <span class="material-symbols-outlined">
+    arrow_back_ios_new
+    </span>
+</p>
 
+  <div class="content">
+
+    <div id="slider" class="slider">
+    </div>
+
+  </div>
+
+  <p data-action="right" class='btn-chevron right bx bx-chevron-right'>
+    <span class="material-symbols-outlined">
+      arrow_forward_ios
+      </span>
+  </p>
+  </div>
+`
 
 var currentProject = 'neuart';
 var currentIndex = 0;
+let i = 0;
+let sliderImage = [];
 
-function openGallery(projectName) {
+
+
+function openContact() {
+  var oldAlbum = document.getElementById(currentProject);
+  oldAlbum.removeAttribute('class');
+
+  var album = document.getElementById("about");
+  album.className = 'selected-album';
+
+  currentProject = 'about';
+
+  var content = document.getElementById("content");
+  content.setHTML(about);
+
+
+}
+
+let resetCarousel = () => {
+  i = 0;
+
+  const content = document.getElementsByClassName('content')[0];
+  if (!content) return;
+  content.setHTML(`<div id="slider" class="slider">
+  </div>`)
+  slider = document.getElementById('slider');
+}
+
+let openGallery = (projectName) => {
+  if (i != 0) {
+    resetCarousel();
+  }
+  // setPosition(0);
 
   var oldAlbum = document.getElementById(currentProject);
   oldAlbum.removeAttribute('class');
@@ -25,7 +98,16 @@ function openGallery(projectName) {
   album.className = 'selected-album';
 
   currentProject = projectName;
-  var slider = document.getElementById('slider');
+  // var slider = document.getElementById('slider');
+  if (document.getElementById('slider') == null) {
+    // slider = document.createElement('div');
+    // slider.id = 'slider';
+    // slider.className = 'slider';
+
+    var content = document.getElementById("content");
+    content.setHTML(gallery);
+    slider = document.getElementById("slider");
+  }
   slider.innerHTML = ""; // Clear existing gallery
 
   // Replace the following with your own logic to fetch images for each project
@@ -34,11 +116,15 @@ function openGallery(projectName) {
   // var projectImages = images[projectName];
   // slider.innerHTML = ''
 
-  for (var i = 0; i < images[projectName].length; i++) {
+  for (var j = 0; j < images[projectName].length; j++) {
     image = document.createElement('img');
-    image.src = 'assets/' + projectName + '/' + images[projectName][i];
-    image.className = 'slider-image'
-    slider.appendChild(image);
+    image.src = 'assets/' + projectName + '/' + images[projectName][j];
+    image.className = 'slider-image';
+
+    let imageContainer = document.createElement('div');
+    imageContainer.appendChild(image);
+    imageContainer.className = 'image-wrap';
+    slider.appendChild(imageContainer);
   }
 
   sliderImage = Array.from(document.querySelectorAll(".slider-image"));
@@ -63,77 +149,87 @@ function checkKey(e) {
     if (e.keyCode == '37') {
       i--;
       moveImage();
-      // currentIndex = (currentIndex - 1) % images[currentProject].length;
-      // enlargedImage.src = images[currentProject][currentIndex];
     }
     else if (e.keyCode == '39') {
       i++;
       moveImage();
-      // currentIndex = (currentIndex + 1) % images[currentProject].length;
-      // enlargedImage.src = images[currentProject][currentIndex];
     }
 
 }
 
 
-
-function enlargeImage(index) {
-
-  var galleryImages = document.querySelectorAll('#gallery img');
-  var gallery = document.getElementById('gallery')
-
-  //var overlay = document.createElement('div');
-  //overlay.className = 'overlay';
-  //overlay.addEventListener('click', closeEnlargedView); 
-
-  var imageContainer = document.createElement('div');
-  imageContainer.className = 'image-container';
-
-
-  enlargedImage = document.createElement('img');
-  console.log(images);
-  enlargedImage.src = images[currentProject][currentIndex];
-  enlargedImage.className = 'enlarged-image';
-  document.onkeydown = checkKey;
-
-
-  var prevButton = document.createElement('span');
-  prevButton.innerHTML = '&#8249;';
-  prevButton.className = 'nav-button';
-  prevButton.addEventListener('click', navigateImages.bind(null, currentIndex - 1));
-
-  var nextButton = document.createElement('span');
-  nextButton.innerHTML = '&#8250;';
-  nextButton.className = 'nav-button';
-  nextButton.addEventListener('click', navigateImages.bind(null, currentIndex + 1));
-
-  imageContainer.appendChild(prevButton);
-  imageContainer.appendChild(enlargedImage);
-  imageContainer.appendChild(nextButton);
-  gallery.appendChild(imageContainer);
-
-  document.body.appendChild(overlay);
-}
-
-function closeEnlargedView() {
-  var overlay = document.querySelector('.overlay');
-  overlay.parentNode.removeChild(overlay);
-  currentIndex = 0;
-}
-
-function navigateImages(index) {
-  var galleryImages = document.querySelectorAll('#gallery img');
-  var maxIndex = galleryImages.length - 1;
-
-  if (index < 0) {
-    index = maxIndex;
-  } else if (index > maxIndex) {
-    index = 0;
-  }
-
-  var currentImage = galleryImages[index];
-  var enlargedImage = document.querySelector('.enlarged-image');
-  enlargedImage.src = currentImage.src;
-}
-
 document.onkeydown = checkKey;
+
+
+
+
+
+const content = document.querySelector(".content");
+let slider = document.querySelector(".slider");
+const btnChevron = document.querySelectorAll(".btn-chevron");
+
+let reset = (container, clase) => {
+    container.forEach(item => item.classList.remove(clase));
+}
+
+let createInfo = text => {
+    const sliderInfo = document.createElement("p");
+    sliderInfo.className = "slider-info";
+    sliderInfo.textContent = text;
+    content.appendChild(sliderInfo);
+};
+
+let createIndicators = () => {
+    const container = document.createElement("div");
+    container.className = "indicator";
+    content.appendChild(container)
+    sliderImage.forEach(image => {
+        let indicator = document.createElement("p");
+        indicator.textContent = sliderImage.indexOf(image) + 1;
+        container.appendChild(indicator);
+    })
+}
+
+let Image = (index) => {
+    const indicators = document.querySelectorAll('.indicator p');
+    sliderImage[index].classList.add('slider-image-active');
+
+}
+
+let setPosition = (index) => {
+    let width = sliderImage[index].getBoundingClientRect().width;
+    slider.style.transform = `translateX(-${width * index}px)`;
+}
+
+let moveImage = () => {
+    if (i === sliderImage.length) {
+        i = 0; // Si el contador ya llego al ultimo item, lo manda al primer item.
+    } else if (i == -1) {
+        i = sliderImage.length - 1; // Si llego al primero lo manda hasta el ultimo.
+    }
+    reset(sliderImage, 'slider-image-active');
+    setPosition(i);
+    Image(i);
+};
+
+btnChevron.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log('ic mi vric');
+        if (btn.dataset.action == "right") {
+            i++;
+            return moveImage();
+        }
+        i--;
+        return moveImage();
+    })
+})
+
+// createIndicators();
+
+
+
+
+window.onload = openGallery('where_you_are');
+
+// let sliderImage = Array.from(document.querySelectorAll(".slider-image"));
+Image(i);
